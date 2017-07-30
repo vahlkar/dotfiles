fa0e03c68d0a7f36b568287a6caaf510d133689a
Volodia Laniel <laniel.volodia@gmail.com>
2017-07-30 16:03:07 +0200

    Update nat scripts with auto detection features.

--- bin/nat_activate.sh
+++ bin/nat_activate.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
-IF_ETH=enp3s0
-IF_WLAN=wlo1
-IF_VPN=tun0
 
 SUBNET="10.0.0.0/24"
 ROUTER="10.0.0.1/24"
 
-IF_OUT="$IF_WLAN"
-
-uid=$(id -u)
-if [ "$uid" -ne 0 ]; then
-    echo "Must be run as root."
+### Default route interface detection
+IF_HOST=$(ip route | grep default | awk '{print $5}')
+if [ -z "$IF_HOST" ]; then
+    echo "Host default interface not detected!" >&2
     exit 1
 fi
+echo "Host default interface on $IF_HOST"
+IF_OUT="$IF_HOST"
 
-echo "Stop netctl-ifplugd@${IF_ETH}.service"
-systemctl stop "netctl-ifplugd@${IF_ETH}.service"
+IF_ETH="$(ip l | grep -B1 "link/ether" | grep "^[0-9]:" | grep -v "$IF_HOST" | awk '{ print $2 }' | sed -e 's/://')"
+if [ -z "$IF_ETH" ]; then
+    echo "Shared interface not detected!" >&2
+    exit 1
+fi
+echo "Ethernet interface on $IF_ETH"
+
+IF_VPN="$(ip l | grep -B1 "link/none" | grep "^[0-9]:" | awk '{ print $2 }' | sed -e 's/://')"
+if [ -z "$IF_VPN" ]; then
+    echo "No VPN"
+    IF_OUT="$IF_HOST"
+else
+    echo "VPN detected on interface $IF_VPN"
+    IF_OUT="$IF_VPN"
+fi
+
+### Root check
+if [ "$EUID" -ne 0 ]; then
+    echo "This script requires root."
+    exit
+fi
+
+ifplugd_active="$(systemctl status | grep -v grep | grep "netctl-ifplugd@${IF_ETH}.service")"
+if [ ! -z "$ifplugd_active" ]; then
+    echo "Stop netctl-ifplugd@${IF_ETH}.service"
+    systemctl stop "netctl-ifplugd@${IF_ETH}.service"
+fi
 
 echo "Configure network interface ${IF_ETH}."
 ip link set "$IF_ETH" down
@@ -27,7 +50,7 @@ echo "Activate IP forward."
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 echo "Reset firewall settings."
-iptables-restore /etc/iptables/iptables.rules
+systemctl restart iptables.service
 
 echo "Allow forward from $SUBNET network."
 iptables -A FORWARD -s "$SUBNET" -m conntrack --ctstate NEW -j ACCEPT
--- bin/nat_deactivate.sh
+++ bin/nat_deactivate.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 IF_ETH=enp3s0
 
-uid=$(id -u)
-if [ "$uid" -ne 0 ]; then
-    echo "Must be run as root."
-    exit 1
+### Root check
+if [ "$EUID" -ne 0 ]; then
+    echo "This script requires root."
+    exit
 fi
 
 echo "Reset firewall settings."
-iptables-restore /etc/iptables/iptables.rules
+systemctl restart iptables.service
 
 echo "Deactivate IP forward."
 echo 0 > /proc/sys/net/ipv4/ip_forward
 
 echo "Stop DHCP Server."
 pkill dhcpd
+
+### Default route interface detection
+IF_HOST=$(ip route | grep default | awk '{print $5}')
+if [ -z "$IF_HOST" ]; then
+    echo "Host default interface not detected!" >&2
+    exit 1
+fi
+echo "Host default interface on $IF_HOST"
+IF_OUT="$IF_HOST"
+
+IF_ETH="$(ip l | grep -B1 "link/ether" | grep "^[0-9]:" | grep -v "$IF_HOST" | awk '{ print $2 }' | sed -e 's/://')"
+if [ -z "$IF_ETH" ]; then
+    echo "Shared interface not detected!" >&2
+    exit 1
+fi
+echo "Ethernet interface on $IF_ETH"
 
 echo "Unconfigure network interface ${IF_ETH}."
 ip link set "$IF_ETH" down
