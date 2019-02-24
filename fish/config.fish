07b575034b7e3cb8067df87390624e3b1dbe6801
Volodia Laniel <laniel.volodia@gmail.com>
2019-02-24 08:52:06 +0100

    Update PATH for ruby.

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,4 @@ setenv VISUAL vim
 setenv DATA /data/$USER
 setenv BORG_REPO "/run/media/volodia/61bf0c76-0358-4114-aa8f-3af3f4c419d2/home/volodia/backup"
 setenv VPN $DATA/tools/nordvpn/ovpn_udp
+set PATH $PATH "$HOME/.gem/ruby/2.4.0/bin/"
