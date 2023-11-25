832c3834afcc6156fbc918d6ab73cdde0d8c79eb
Volodia Laniel <volodia@laniel.eu>
2023-11-25 23:14:22 +0100

    Update fish config and start gpg-agent.

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,19 @@
-setenv GPG_TTY (tty)
-setenv EDITOR vim
-setenv VISUAL vim
-setenv BORG_REPO "/run/media/volodia/61bf0c76-0358-4114-aa8f-3af3f4c419d2/home/volodia/backup"
-setenv VPN $HOME/work/tools/nordvpn/ovpn_udp
-set PATH $PATH "$HOME/.gem/ruby/2.7.0/bin"
-setenv GEM_HOME "$HOME/.gems"
-set PATH $PATH "$GEM_HOME/bin"
-set PATH $PATH "$HOME/Android/Sdk/emulator"
+set -x GPG_TTY (tty)
+set -x EDITOR vim
+set -x VISUAL vim
+set -x BORG_REPO "/run/media/volodia/61bf0c76-0358-4114-aa8f-3af3f4c419d2/home/volodia/backup"
+set -x VPN $HOME/work/tools/nordvpn/ovpn_udp
+set -x GEM_HOME "$HOME/.gems"
+set -x GRIM_DEFAULT_DIR "$HOME/Pictures/Autres/Screenshots/"
+
+fish_add_path "$HOME/bin"
+fish_add_path "$GEM_HOME/bin"
+fish_add_path "$HOME/.gem/ruby/3.0.0/bin"
+fish_add_path "$HOME/Android/Sdk/emulator"
+
+if status --is-login
+    # GPG Agent
+    gpg-connect-agent /bye
+    #export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+    set -x SSH_AUTH_SOCK "/run/user/$(id -u)/gnupg/S.gpg-agent.ssh"
+end
