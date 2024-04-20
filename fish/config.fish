be7999118de56c26576f06663ee715ccd8ee3169
Volodia Laniel <volodia@laniel.eu>
2024-04-20 11:48:42 +0200

    Update screenshot configuration.

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ set -x VISUAL vim
 set -x BORG_REPO "/run/media/volodia/61bf0c76-0358-4114-aa8f-3af3f4c419d2/home/volodia/backup"
 set -x VPN $HOME/work/tools/nordvpn/ovpn_udp
 set -x GEM_HOME "$HOME/.gems"
-set -x GRIM_DEFAULT_DIR "$HOME/Pictures/Autres/Screenshots/"
+set -x GRIM_DEFAULT_DIR "$HOME/Pictures/Screenshots/"
 
 fish_add_path "$HOME/bin"
 fish_add_path "$GEM_HOME/bin"
